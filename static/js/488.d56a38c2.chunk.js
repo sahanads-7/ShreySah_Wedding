13f65863b036f044a7658fe931a862465b186450
sahanads-7 <sahanads7@example.com>
2026-04-22 15:14:59 +0530

    final clean deploy

---
+++ static/js/488.d56a38c2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkwedding_invitation=globalThis.webpackChunkwedding_invitation||[]).push([[488],{2488(i,n,e){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.d56a38c2.chunk.js.map
